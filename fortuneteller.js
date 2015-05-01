8e29f6fe57ce388e1861b168f8c29fcb32803502
unknown <AnnaE@ncmls8065.ncmls.org>
2015-04-30 22:14:03 -0400

    1st homework done

---
+++ fortuneteller.js
@@ -0,0 +1,9 @@
+          var numberOfChildren = 5;
+          var nameOfPartner = 'Jack Black ';
+          var geographicLocation = 'Germany';
+          var jobTitle = 'Candy Tester ';
+          document.write('You will be a ' + jobTitle + 'in ' +
+                          geographicLocation + ', and married to ' +
+                          nameOfPartner + 'with ' + numberOfChildren
+                          + ' kids.');
+          alert('ZACH SMELLS');
---
+++ mycode.js
@@ -0,0 +1,6 @@
+           function whatUp (response){
+                console.log('What\'s up? ' + response);
+            }
+            whatUp ('Nothing much.');
+            whatUp ('Everything!');
+            whatUp ('Go away.');
